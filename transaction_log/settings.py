7ae4e6ce08ece88e9f8cf6923e541e0ba5f26c4e
Leoni Mella <leoni.mella@gmail.com>
2023-02-25 12:55:43 -0300

    feat: added transaction model

--- transaction_log/settings.py
+++ transaction_log/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'transactions.apps.TransactionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Transaction Model
+class Transaction(models.Model):
+    class TransactionTypes(models.IntegerChoices):
+        DEPOSIT = 1
+        WITHDRAW = 2
+        EXPENSE = 3
+
+    type = models.IntegerField(choices=TransactionTypes.choices)
+    value = models.PositiveBigIntegerField()
+    merchant = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "id " + str(self.id)
